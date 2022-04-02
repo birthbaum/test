b439a880665622539328da6e5a244d5ef4b752a3
birthbaum <64859455+birthbaum@users.noreply.github.com>
2022-04-02 18:27:33 -0500

    Add files via upload

--- my.js
+++ my.js
@@ -11,4 +11,25 @@
     });
 }
 
+ function addGS(){
+ 
+ const url = "https://script.googleusercontent.com/macros/echo?user_content_key=sfGESt-hkZt0iCOsR2hF3UijrkkEUTjXqYOxfbDn_EofXoZLrotHusGtZWNzsOlHaMeHOq8Iqf2ciG9J3rQhoVftjyWZAQfWm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEBNX8UK0M7wk3JuCWV9iij8Mq3dyNLK2CGM6wPTSj0f7Ztfdm7c12z9tphHG-Jrw9p4ItwK-TwZyQ1VvSIM_T9Gp3uZWLYbANz9Jw9Md8uu&lib=MidLq2kEFqQ3BSnYFuEuNbwxC__QRcjdY";
+
+    fetch(url,{
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({name: "Olivia"})
+ });
+  
+}
+
+
+document.getElementById("btn2").addEventListener("click", addGS);
+
 document.getElementById("btn").addEventListener("click", testGS);
