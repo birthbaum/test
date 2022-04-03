8a4827df8a2b65aea34cb5ddd433b66742121685
birthbaum <64859455+birthbaum@users.noreply.github.com>
2022-04-02 22:21:31 -0500

    Add files via upload

--- my.js
+++ my.js
@@ -1,4 +1,4 @@
- function testGS(){
+function testGS(){
  
  const url = "https://script.google.com/macros/s/AKfycbwy8Zc4zEvICVHr-dKe0E9IeYbNDBzPTjMqFSlfBQ_3T67kXUntCz86NMc8i2zK5iEO1g/exec";
 
@@ -23,7 +23,7 @@
             'Content-Type': 'application/json'
         },
         redirect: 'follow',
-        body: JSON.stringify({"Student Name":"Eddie","Gender":"Male","Class Level":"4. Senior","State":"IL","Major":"Math","Extracurricular Activity":"Business Intelligence"})
+        body: JSON.stringify({"Student Name":fName,"Gender":"Male","Class Level":"4. Senior","State":"IL","Major":"Math","Extracurricular Activity":"Business Intelligence"})
  });
   
 }
@@ -32,3 +32,5 @@
 document.getElementById("btn2").addEventListener("click", addGS);
 
 document.getElementById("btn").addEventListener("click", testGS);
+
+const fName = document.getElementById("inputfname");
